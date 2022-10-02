08079d8576c00e015625e7e0151750bb9d1e5a38
bkrmadtya <karki.bikram007@gmail.com>
2022-10-02 18:18:36 +0300

    feat: added deploy script for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/battle-of-state-managers',
   plugins: [react()],
   publicDir: './public',
   resolve: {
